34064119d56f86f20200d1b5dfeecd04f5ccf6b5
Martin Recher <martin.recher@etu.uca.fr>
2023-11-01 15:32:52 +0100

    a

--- src/controller/ServerController.java
+++ src/controller/ServerController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ServerController {
+public class ServerController implements Initializable {
 	
 	@FXML
 	Label server, ipAddress, valueIPAddress, port, valuePort, playersConnected, nbPlayersConnected, labelNewPort;
@@ -15,4 +19,10 @@ public class ServerController {
 	
 	@FXML
 	Button confirm;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
